ac1e2e58e3e200eeb23bcb05122f47df2e943453
Bona Hirpa <bonaforg2@gmail.com>
2025-03-15 09:01:13 +0300

    setUps added

---
+++ Star-Rating/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+import StarRating from "./starRating/StarRating";
+const App = () => {
+  return (
+    <>
+      <StarRating />
+    </>
+  );
+};
+
+export default App;
